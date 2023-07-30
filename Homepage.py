5bc06db74f165d30f43c559135cd029c4f5c0538
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-07-30 17:01:54 +0530

    Update Homepage

--- Homepage.py
+++ Homepage.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 
 
+movies_data = pd.read_csv("https://raw.githubusercontent.com/danielgrijalva/movie-stats/7c6a562377ab5c91bb80c405be50a0494ae8e582/movies.csv")
+
+movies_data.info()
+
+movies_data.duplicated()
+
+movies_data.count()
+
+movies_data.dropna()
 
 
 st.set_page_config(
@@ -11,12 +21,31 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.title("Yellow Root")
-st.sidebar.success("Select a page above.")
-image = Image.open('top.jpg')
+st.write("""Average Movie Budget, Grouped by Genre""")
+avg_budget = movies_data.groupby('genre')['budget'].mean().round()
+avg_budget = avg_budget.reset_index()
+genre = avg_budget['genre']
+avg_bud = avg_budget['budget']
 
-st.image(image, caption='@Yellow Root')
-st.markdown('Welcome to YellowRoot, a smart and intelligent system designed to maximize turmeric cultivation in Sri Lanka. Our web app offers four essential components powered by machine learning to revolutionize the turmeric industry. Whether youre a farmer, researcher, or enthusiast, YellowRoot is here to provide you with valuable insights and recommendations for enhancing turmeric growth. ')
-st.markdown('1.Disease Identification and Recommendations: With our advanced machine learning algorithms, YellowRoot can accurately identify and classify turmeric leaf diseases. By uploading images of diseased leaves, our system quickly analyzes the symptoms and provides tailored recommendations for disease management. This helps farmers take proactive measures to prevent crop loss and optimize turmeric health.')
-st.markdown('2.Turmeric Quality Prediction and Grading: YellowRoot brings intelligence to turmeric quality assessment. By leveraging machine learning models, we analyze the visual features of turmeric rhizomes. Our system predicts the quality and grades the turmeric based on various parameters such as color, size, and texture. This empowers farmers and traders to make informed decisions, ensuring high-quality produce reaches the market.')
-st.markdown('3.Dryness Level Prediction and Powdering Recommendations: Timing is crucial when it comes to drying turmeric for powdering. YellowRoot s machine learning algorithms forecast the dryness level of turmeric powders. By considering factors such as moisture content and environmental conditions, our system suggests the best time for powdering turmeric. This helps in maintaining optimal quality and maximizing the flavor and potency of the powder.')
+fig = plt.figure(figsize = (19, 10))
+
+plt.bar(genre, avg_bud, color = 'maroon')
+plt.xlabel('genre')
+plt.ylabel('budget')
+plt.title('Matplotlib Bar Chart Showing the Average \
+Budget of Movies in Each Genre')
+
+st.pyplot(fig)
+
+col1, col2 = st.columns(2)
+col1.write('# This is Column 1')
+col2.write('# This is Column 2')
+
+st.write('### Columns of different sizes')
+col1, col2, col3, col4 = st.columns([1,3,1,2])
+
+col1.write('# This is Column 1')
+col2.write('# This is Column 2')
+col3.write('# This is Column 3')
+col4.write('# This is Column 4')
+
