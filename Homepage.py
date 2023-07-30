064999aeb14dca981c4766132a93f6ccf34c679a
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-07-30 17:13:29 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -55,20 +55,12 @@ genre_list = movies_data['genre'].unique().tolist()
 year_list = movies_data['year'].unique().tolist()
 
 with st.sidebar:
-       st.write("Select a range on the slider (it represents movie score) \
-       to view the total number of movies in a genre that falls \
-       within that range ")
+    st.write("Select a range on the slider (it represents movie score) \to view the total number of movies in a genre that falls \ within that range ")
     #create a slider to hold user scores
-    new_score_rating = st.slider(label = "Choose a value:",
-                                  min_value = 1.0,
-                                  max_value = 10.0,
-                                 value = (3.0,4.0))
+    new_score_rating = st.slider(label = "Choose a value:",min_value = 1.0,max_value = 10.0,value = (3.0,4.0))
 
 #create a multiselect widget to display genre
-new_genre_list = st.multiselect('Choose Genre:',
-                                        genre_list, default = ['Animation',\
-                                         'Horror',  'Fantasy', 'Romance'])
+new_genre_list = st.multiselect('Choose Genre:',genre_list, default = ['Animation','Horror',  'Fantasy', 'Romance'])
 #create a selectbox option that holds all unique years
-year = st.selectbox('Choose a Year',
-    year_list, 0)
+year = st.selectbox('Choose a Year',year_list, 0)
 
