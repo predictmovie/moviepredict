c2dcc5f4df8803eca6a1a3a3befb7a805590425c
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-06 12:03:18 +0530

    Update Movie Predict.py

--- pages/Movie Predict.py
+++ pages/Movie Predict.py
@@ -32,24 +32,47 @@ if uploaded_file is not None:
     # img_reshape = resized[np.newaxis,...]
     video_pred = st.button("Set Video")  
     if video_pred:
-        myFrameNumber = 50
-        cap = cv2.VideoCapture("video.mp4")
+        cap = cv2.VideoCapture(video_name)
 
-    # get total number of frames
-        totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+#Set frame_no in range 0.0-1.0
+#In this example we have a video of 30 seconds having 25 frames per seconds, thus we have 750 frames.
+#The examined frame must get a value from 0 to 749.
+#For more info about the video flags see here: https://stackoverflow.com/questions/11420748/setting-camera-parameters-in-opencv-python
+#Here we select the last frame as frame sequence=749. In case you want to select other frame change value 749.
+#BE CAREFUL! Each video has different time length and frame rate. 
+#So make sure that you have the right parameters for the right video!
+        time_length = 30.0
+        fps=25
+        frame_seq = 749
+        frame_no = (frame_seq /(time_length*fps))
 
-    # check for valid frame number
-        if myFrameNumber >= 0 & myFrameNumber <= totalFrames:
-    # set frame position
-            cap.set(cv2.CAP_PROP_POS_FRAMES,myFrameNumber)
+#The first argument of cap.set(), number 2 defines that parameter for setting the frame selection.
+#Number 2 defines flag CV_CAP_PROP_POS_FRAMES which is a 0-based index of the frame to be decoded/captured next.
+#The second argument defines the frame number in range 0.0-1.0
+        cap.set(2,frame_no);
 
-        while True:
-            ret, frame = cap.read()
-            cv2.imshow("Video", frame)
-            if cv2.waitKey(20) & 0xFF == ord('q'):
-                break
+#Read the next frame from the video. If you set frame 749 above then the code will return the last frame.
+        ret, frame = cap.read()
 
+#Set grayscale colorspace for the frame. 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#Cut the video extension to have the name of the video
+        my_video_name = video_name.split(".")[0]
+
+#Display the resulting frame
+        cv2.imshow(my_video_name+' frame '+ str(frame_seq),gray)
+
+#Set waitKey 
+        cv2.waitKey()
+
+#Store this frame to an image
+        cv2.imwrite(my_video_name+'_frame_'+str(frame_seq)+'.jpg',gray)
+
+# When everything done, release the capture
+        cap.release()
         cv2.destroyAllWindows()
+
 
     Genrate_pred = st.button("Leaf Predict")    
     if Genrate_pred:
