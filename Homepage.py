ec45baf33b32c8f1dcda714b72a95e5769515984
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-20 16:01:06 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -39,26 +39,3 @@ Budget of Movies in Each Genre')
 st.pyplot(fig)
 
 
-# Creating sidebar widget unique values from our movies dataset
-score_rating = movies_data['score'].unique().tolist()
-genre_list = movies_data['genre'].unique().tolist()
-year_list = movies_data['year'].unique().tolist()
-
-with st.sidebar:
-    st.write("Select a range on the slider (it represents movie score) \to view the total number of movies in a genre that falls \ within that range ")
-    #create a slider to hold user scores
-    new_score_rating = st.slider(label = "Choose a value:",min_value = 1.0,max_value = 10.0,value = (3.0,4.0))
-
-#create a multiselect widget to display genre
-new_genre_list = st.multiselect('Choose Genre:',genre_list, default = ['Animation','Horror',  'Fantasy', 'Romance'])
-#create a selectbox option that holds all unique years
-year = st.selectbox('Choose a Year',year_list, 0)
-
-#Configure and filter the slider widget for interactivity
-score_info = (movies_data['score'].between(*new_score_rating))
-#Filter the selectbox and multiselect widget for interactivity
-new_genre_year = (movies_data['genre'].isin(new_genre_list)) & (movies_data['year'] == year)
-
-# visualization section
-#group the columns needed for visualizations
-
