dd1941b35127e867f27afe911f0505866c239db4
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-22 12:00:28 +0530

    Update Movie Predict.py

--- pages/Movie Predict.py
+++ pages/Movie Predict.py
@@ -60,7 +60,7 @@ if uploaded_file is not None:
               st.image(pil_img)
               cur_frame += 1
               file_bytes = np.asarray(frame, dtype=np.uint8)
-              opencv_image = cv2.imread(file_bytes,1)
+              opencv_image = cv2.imread(pil_img,1)
               opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
               resized = cv2.resize(opencv_image,(224,224))
               st.image(opencv_image, channels="RGB")
