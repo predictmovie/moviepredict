d4d7b5d3dad0d60e5ecb322fd49f3d0793ea3009
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-22 15:33:21 +0530

    Update Movie Predict.py

--- pages/Movie Predict.py
+++ pages/Movie Predict.py
@@ -10,7 +10,7 @@ model = tf.keras.models.load_model("saved_model/movie.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a file")
 st.write(uploaded_file)
-frame_skip = 300
+frame_skip = 10000
 
 map_dict = {0:'Action',
             1:'Comedy',
