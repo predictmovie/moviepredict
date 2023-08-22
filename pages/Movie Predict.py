3ddc62b51655fd4d39e80d939e26582362791720
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-22 11:42:16 +0530

    Update Movie Predict.py

--- pages/Movie Predict.py
+++ pages/Movie Predict.py
@@ -62,7 +62,7 @@ if uploaded_file is not None:
               file_bytes = np.asarray(bytearray(frame), dtype=np.uint8)
               opencv_image = cv2.imdecode(file_bytes, 1)
               # opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-              resized = cv2.resize(opencv_image,(224,224))
+              # resized = cv2.resize(opencv_image,(224,224))
               st.image(opencv_image, channels="RGB")
               resized = mobilenet_v2_preprocess_input(resized)
               img_reshape = resized[np.newaxis,...]
