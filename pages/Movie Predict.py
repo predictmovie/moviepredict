d71972d0a4e4715859cb77c2727d14838406469d
predictmovie <140945520+predictmovie@users.noreply.github.com>
2023-08-22 11:48:06 +0530

    Update Movie Predict.py

--- pages/Movie Predict.py
+++ pages/Movie Predict.py
@@ -59,10 +59,10 @@ if uploaded_file is not None:
               pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
               st.image(pil_img)
               cur_frame += 1
-              file_bytes = np.asarray(bytearray(frame), dtype=np.uint8)
-              opencv_image = cv2.imdecode(file_bytes, 1)
-              # opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-              # resized = cv2.resize(opencv_image,(224,224))
+              # file_bytes = np.asarray(bytearray(frame), dtype=np.uint8)
+              opencv_image = cv2.imdecode(pil_img)
+              opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+              resized = cv2.resize(opencv_image,(224,224))
               st.image(opencv_image, channels="RGB")
               resized = mobilenet_v2_preprocess_input(opencv_image)
               img_reshape = resized[np.newaxis,...]
